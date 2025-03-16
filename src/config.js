7788fb89d93fc2d183db00b404d2d2eb7644b859
Jayesh shukla <jayeshshukla17@gmail.com>
2025-03-16 18:10:45 +0000

    sepolia base done api

--- src/config.js
+++ src/config.js
@@ -12,6 +12,10 @@ const config = {
       name: "Polygon Mainnet",
       rpc: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_POLYGON_KEY}`,
     },
+    sepoliaBase: {
+      name: "Sepolia Base",
+      rpc: `https://base-mainnet.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_SEPOLIA_BASE_KEY}`,
+    },
   },
 };
 
