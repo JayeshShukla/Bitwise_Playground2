bf1c3ee8e31a7f962f07e441f93b965890f2ebbf
haildlord <hailthelord16@gmail.com>
2026-08-06 16:59:58 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Buffer } from "buffer";
-window.Buffer = window.Buffer || Buffer;
-
 import App from "./App";
+
+window.Buffer = window.Buffer || Buffer;
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
